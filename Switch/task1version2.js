d3058380e4f065170380f175540a7c1624f85385
drradicevic <drradicevic@gmail.com>
2021-03-12 20:52:22 +0100

    Finish switch exercise

---
+++ Switch/task1version2.js
@@ -0,0 +1,29 @@
+/* 1. Write a program that shows text representation of a day in a week for a number input from
+1 to 7. Print output in console.
+For input 1, output should be “Monday”. */
+
+var day = 3;
+
+switch (day) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Thuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  case 4:
+    console.log("Thursday");
+    break;
+  case 5:
+    console.log("Friday");
+    break;
+  case 6:
+    console.log("Saturday");
+    break;
+  default:
+    console.log("Sunday");
+    break;
+}
