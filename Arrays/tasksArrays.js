09e9888e2454bf1997ccb103181bbee0f0969338
drradicevic <drradicevic@gmail.com>
2021-03-14 15:04:48 +0100

    Finish tasks for Arrays and Switch

---
+++ Arrays/tasksArrays.js
@@ -0,0 +1,67 @@
+/* 1. Write an array of months in a year. Using console.log display June, October and January
+from the array.   
+
+var months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+console.log(months[5], months[9], months[0]);    */
+
+/*  2. Write an arrays of days in a week. Using console.log display Sunday from the array.  
+
+var day = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+console.log(day[6]);   */
+
+/* 3. Print all negative elements from the array [2, -4, 5, -2, -11].
+ 
+var number = [2, -4, 5, -2, -11];
+console.log(
+  "All negative elements from the array: " + number[1],
+  number[3],
+  number[4]
+);   */
+
+/* 4. Print all elements with indices divisible by 3 from the array [5, 15, -5, 20, 12, 18, 72, 14, 9].
+ 
+var num = [5, 15, -5, 20, 12, 18, 72, 14, 9];
+console.log("Indices devisible by 3 are: " + num[0], num[3], num[6]);    */
+
+/*  5. What is the index of number 24 in the following array?
+[[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3,
+10, 26, 7]];
+Using console.log:
+● Display the 3rd element of the array,
+● Display the 2nd element of the 3rd element.    
+
+var num = [
+  [1, 2, 1, 24],
+  [8, 11, 9, 4],
+  [7, 0, 7, 27],
+  [7, 4, 28, 14],
+  [3, 10, 26, 7],
+];
+
+console.log(num[0][3]);
+
+console.log(num[2]);
+
+console.log(num[2][1]);    */
