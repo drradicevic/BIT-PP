62134cd61c75d2f6c34385a024278c3643b8d950
drradicevic <drradicevic@gmail.com>
2021-03-29 19:37:49 +0200

    task10 edited

--- Functions/task10.js
+++ Functions/task10.js
@@ -1,15 +1,13 @@
 /*  10. Write a program that calculates the number of appearances of a letter a in a given string.
 Modify the program so it calculates the number of both letters a and A.  */
 
-function numberOfAppearance(string, number) {
+function calculates(string, letteraA) {
   var count = 0;
-
   for (var i = 0; i < string.length; i++) {
-    if (string[i] === number) {
+    if (string[i] === "a" || string[i] === "A") {
       count++;
     }
   }
   return count;
 }
-
-console.log(numberOfAppearance("dragan radicevic", "a"));
+console.log(calculates("dragan RAdicevic", "a"));
