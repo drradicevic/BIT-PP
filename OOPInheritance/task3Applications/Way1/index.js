98e9ce25e835a6ccfb2048e3f47d0dabb4c0c7c2
drradicevic <drradicevic@gmail.com>
2021-06-14 15:49:41 +0200

    OOP inheritance task3Applications finished

---
+++ OOPInheritance/task3Applications/Way1/index.js
@@ -0,0 +1,124 @@
+/*
+Applications:
+1. Create constructor functions with properties representing the following:
+○ WebApp: name, url, technologies, licence, stars
+○ MobileApp: name, platforms, licence, stars
+2. All web applications should inherit methods:
+○ getData which prints out all the information
+○ reactBased which checks if one of the used technologies is React
+3. All mobile applications should inherit methods:
+○ getData which prints out all the informations
+○ forAndroid which checks if one of the platforms the application is developed for is
+Android
+4. Both web and mobile applications should inherit methods:
+○ isCCLicence which checks if the licence of the application is CC (Creative
+Commons)
+○ like which increases the number of stars by one
+○ showStars which prints out the number of stars
+*/
+
+function Application(name, licence, stars) {
+  this.name = name;
+  this.licence = licence;
+  this.stars = stars;
+}
+
+function WebApp(name, url, technologies, licence, stars) {
+  Application.call(this, name, licence, stars);
+  this.url = url;
+  this.technologies = technologies;
+}
+WebApp.prototype = Object.create(Application.prototype);
+WebApp.prototype.constructor = WebApp;
+
+WebApp.prototype.getData = function () {
+  return (
+    this.name +
+    " " +
+    this.url +
+    " " +
+    this.technologies +
+    " " +
+    this.licence +
+    " " +
+    this.stars
+  );
+};
+
+WebApp.prototype.reactBased = function () {
+  if (this.technologies === "React" || this.technologies === "react") {
+    return "Technology  which used  is " + this.technologies;
+  } else {
+    return "Technology  which used  is not React";
+  }
+};
+
+WebApp.prototype.isCCLicence = function () {
+  if (this.licence === "CC") {
+    return "This licence is " + this.licence;
+  } else {
+    return "This licence is not CC ";
+  }
+};
+
+WebApp.prototype.like = function () {
+  return this.stars++;
+};
+
+WebApp.prototype.showStars = function () {
+  return this.stars;
+};
+
+function MobileApp(name, platforms, licence, stars) {
+  Application.call(this, name, licence, stars);
+  this.platforms = platforms;
+}
+MobileApp.prototype = Object.create(Application.prototype);
+MobileApp.prototype.constructor = MobileApp;
+
+MobileApp.prototype.getData = function () {
+  return (
+    this.name + " " + this.platforms + " " + this.licence + " " + this.stars
+  );
+};
+
+MobileApp.prototype.forAndroid = function () {
+  if (this.platforms === "Android" || this.platforms === "android") {
+    return "Technology  which used  is Android";
+  } else {
+    return "Technology  which used  is not Android";
+  }
+};
+
+MobileApp.prototype.isCCLicence = function () {
+  if (this.licence === "CC") {
+    return "This licence is " + this.licence;
+  } else {
+    return "This licence is not CC ";
+  }
+};
+
+MobileApp.prototype.like = function () {
+  return this.stars++;
+};
+
+MobileApp.prototype.showStars = function () {
+  return this.stars;
+};
+
+var web = new WebApp("Soccer", "www.soccer.rs", "React", "CC", 4);
+var mobile = new MobileApp("Note", "android", "CCC", 5);
+
+console.log(web);
+console.log(web.getData());
+console.log(web.reactBased());
+console.log(web.isCCLicence());
+console.log(web.like());
+console.log(web.showStars());
+
+console.log(mobile);
+console.log(mobile.getData());
+console.log(mobile.forAndroid());
+console.log(mobile.isCCLicence());
+console.log(mobile.like());
+console.log(mobile.showStars());
