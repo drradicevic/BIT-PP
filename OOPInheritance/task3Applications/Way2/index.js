c9779807e15289692b90b7a887856633b12d2a5f
drradicevic <drradicevic@gmail.com>
2021-06-18 11:53:36 +0200

    OOP update

--- OOPInheritance/task3Applications/Way2/index.js
+++ OOPInheritance/task3Applications/Way2/index.js
@@ -1,21 +1,21 @@
-/*
-Applications:
-1. Create constructor functions with properties representing the following:
-○ WebApp: name, url, technologies, licence, stars
-○ MobileApp: name, platforms, licence, stars
-2. All web applications should inherit methods:
-○ getData which prints out all the information
-○ reactBased which checks if one of the used technologies is React
-3. All mobile applications should inherit methods:
-○ getData which prints out all the informations
-○ forAndroid which checks if one of the platforms the application is developed for is
-Android
-4. Both web and mobile applications should inherit methods:
-○ isCCLicence which checks if the licence of the application is CC (Creative
-Commons)
-○ like which increases the number of stars by one
-○ showStars which prints out the number of stars
-*/
+// /*
+// Applications:
+// 1. Create constructor functions with properties representing the following:
+// ○ WebApp: name, url, technologies, licence, stars
+// ○ MobileApp: name, platforms, licence, stars
+// 2. All web applications should inherit methods:
+// ○ getData which prints out all the information
+// ○ reactBased which checks if one of the used technologies is React
+// 3. All mobile applications should inherit methods:
+// ○ getData which prints out all the informations
+// ○ forAndroid which checks if one of the platforms the application is developed for is
+// Android
+// 4. Both web and mobile applications should inherit methods:
+// ○ isCCLicence which checks if the licence of the application is CC (Creative
+// Commons)
+// ○ like which increases the number of stars by one
+// ○ showStars which prints out the number of stars
+// */
 
 class Applications {
   constructor(name, licence, stars) {
