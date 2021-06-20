1066ab8604bb314683fab2b35daaefb5b0bfe32f
drradicevic <drradicevic@gmail.com>
2021-06-20 09:29:55 +0200

    Practise

--- vezba.js
+++ vezba.js
@@ -9,3 +9,8 @@ function someFunctions(someExercise, num3) {
 
 var result = someFunctions(someExercise(5, 8), 20);
 console.log(result);
+
+function random(number) {
+  return number || Math.floor(91 * Math.random() + 10);
+}
+console.log(random(10));
