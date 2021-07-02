0657c518e66a6a593a3d25c71f46f21f964df2be
drradicevic <drradicevic@gmail.com>
2021-07-02 21:19:04 +0200

    Update

--- test.js
+++ test.js
@@ -5,3 +5,20 @@ function f() {
 }
 var result = f(2, 3);
 console.log(result);
+
+/* Write a function to count the number of letter occurrences in a string.
+My random string; n ==>  2  */
+
+function someFunction(array, string) {
+  var count = 0;
+  for (var i = 0; i < array.length; i++) {
+    if (string === array[i]) {
+      count += 1;
+    }
+  }
+  return count;
+}
+
+var result = someFunction("My random string", "n");
+
+console.log(result);
