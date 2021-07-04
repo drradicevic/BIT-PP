9690a2c7b427d2dde08aa819028a153668c936cb
drradicevic <drradicevic@gmail.com>
2021-07-04 13:56:38 +0200

    Update

--- vezba.js
+++ vezba.js
@@ -1,16 +1,51 @@
-function someExercise(num1, num2) {
-  var sum = num1 * num2;
-  return sum;
+function Person(name, lastName) {
+  if (!name) {
+    throw new Error("This input is required!");
+  }
+  if (!lastName) {
+    throw new Error("This input is required!");
+  }
+  this.name = name;
+  this.lastName = lastName;
+
+  this.getData = function () {
+    return `${this.name} ${this.lastName}`;
+  };
 }
 
-function someFunctions(someExercise, num3) {
-  return someExercise - num3;
+function Seat(number = Math.floor(91 * Math.random() + 10), category = "e") {
+  if (!["e", "b"].includes(category)) {
+    throw new Error("This input is required!");
+  }
+
+  this.number = number;
+  this.category = category;
+  this.getData = function () {
+    return `${this.number}, ${this.category.toUpperCase()}`;
+  };
 }
 
-var result = someFunctions(someExercise(5, 8), 20);
-console.log(result);
+function Passenger(person, seat) {
+  if (!person || !(person instanceof Person)) {
+    throw new Error("Invalid person input");
+  }
+  if (!seat || !(seat instanceof Seat)) {
+    throw new Error("Invalid person input");
+  }
+  this.person = person;
+  this.seat = seat;
 
-function random(number) {
-  return number || Math.floor(91 * Math.random() + 10);
+  this.getData = function () {
+    return `Passenger: ${this.person.getData()}, has return card for: ${this.seat.getData()}`;
+  };
 }
-console.log(random(10));
+
+let person = new Person("Dragan", "Radicevic");
+console.log(person.getData());
+
+let seat = new Seat(25, "b");
+console.log(seat.getData());
+
+let passenger = new Passenger(person, seat);
+
+console.log(passenger.getData());
