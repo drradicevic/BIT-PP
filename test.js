79eec82bb64b62e1ff90afc207701a64ad2c2ec4
drradicevic <drradicevic@gmail.com>
2021-07-05 18:58:44 +0200

    Update

--- test.js
+++ test.js
@@ -22,3 +22,21 @@ function someFunction(array, string) {
 var result = someFunction("My random string", "n");
 
 console.log(result);
+
+/* 2.	Write a program that multiplies every positive element of a given array by 2.
+Input array: [-3, 11, 5, 3.4, -8]
+Output array: [-3, 22, 10, 6.8, -8]    */
+
+function multiples(array) {
+  var output = [];
+  for (i = 0; i < array.length; i++) {
+    if (array[i] > 0) {
+      output[output.length] = array[i] * 2;
+    } else {
+      output[output.length] = array[i];
+    }
+  }
+  return output;
+}
+var result = multiples([-3, 11, 5, 3.4, -8]);
+console.log(result);
