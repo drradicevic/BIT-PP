b721de383359ba88825eac4d3139d29403d7ba85
drradicevic <drradicevic@gmail.com>
2021-07-09 14:01:06 +0200

    Task 3

---
+++ ES6/task3.js
@@ -0,0 +1,15 @@
+// 3.	Write a function that increases each element of the given array by the given value. If the value is omitted, increase each element of the array by 1.
+// 	Input: [4, 6, 11, -9, 2.1], 2
+// 	Output: [6, 8, 13, -7, 4.1]
+
+function increasesEachElement(array, number) {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    newArray[newArray.length] = array[i] + number;
+  }
+  return newArray;
+}
+
+let result = increasesEachElement([4, 6, 11, -9, 2.1], 2);
+
+console.log(result);
